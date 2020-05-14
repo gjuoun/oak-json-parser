59a926d54995a3b9e5b347e786e938822165734c
gjuoun <gjuoun@gmail.com>
2020-05-14 04:04:04 -0300

    update json value

--- lib/json.ts
+++ lib/json.ts
@@ -2,7 +2,7 @@ import { Context } from "https://deno.land/x/oak/mod.ts";
 
 declare module "https://deno.land/x/oak/mod.ts" {
   interface Request {
-    json: Record<string, string>
+    json: Record<string, string>,
   }
 }
 
@@ -12,7 +12,7 @@ export function jsonParser(options: any = null) {
     if (ctx.request.hasBody) {
       try {
         let jsonObj = await ctx.request.body({ contentTypes: { json: [] } })
-        ctx.request.json = <Record<string, string>>jsonObj
+        ctx.request.json = <Record<string, string>>jsonObj.value
         next()
       }
       catch (e) {
